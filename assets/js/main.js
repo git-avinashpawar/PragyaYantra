6bf0eab19b8c94f4b1065ffa090606832aaf1243
git-avinashpawar <cloudmail.project.x@gmail.com>
2024-09-16 00:21:00 -0400

    fix: mobile spacings

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,3 +182,15 @@ function showDiv(element) {
   // document.getElementById(divId).style.display =
   //   element.value == "Library" ? "block" : "none";
 }
+
+// First  get the viewport height and  multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then  set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+//  listen to the resize event
+window.addEventListener("resize", () => {
+  // execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
